255ced2c8268f22771c2e7ec4c8d85deb25b27f7
Almeida <isabel.sales.almeida@accenture.com>
2020-06-04 22:05:00 -0300

    Inclusão de arquivos

---
+++ js/main.js
@@ -0,0 +1,128 @@
+jQuery(document).ready(function( $ ) {
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.smothscroll').on('click', function(e) {
+    e.preventDefault();
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - 62
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $('.carousel').carousel({
+    interval: 3500
+  });
+
+  // JavaScript Chart
+  var doughnutData = [{
+      value: 80,
+      color: "#1abc9c"
+    },
+    {
+      value: 20,
+      color: "#ecf0f1"
+    }
+  ];
+  var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+
+  // Bootstrap Chart
+  var doughnutData = [{
+    value: 90,
+    color: "#1abc9c"
+  },
+  {
+    value: 10,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
+
+  // WordPress Chart
+  var doughnutData = [{
+    value: 65,
+    color: "#1abc9c"
+  },
+  {
+    value: 35,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("wordpress").getContext("2d")).Doughnut(doughnutData);
+
+  // HTML Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("html").getContext("2d")).Doughnut(doughnutData);
+
+  // NodeJs Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("NodeJs").getContext("2d")).Doughnut(doughnutData);
+
+  // Python Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("Python").getContext("2d")).Doughnut(doughnutData);
+  
+  // ReactJs Chart
+  var doughnutData = [{
+    value: 60,
+    color: "#1abc9c"
+  },
+  {
+    value: 40,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("ReactJs").getContext("2d")).Doughnut(doughnutData);
+  
+  // ReactNative Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("ReactNative").getContext("2d")).Doughnut(doughnutData);
+  
+  // OpenCV Chart
+  var doughnutData = [{
+    value: 60,
+    color: "#1abc9c"
+  },
+  {
+    value: 40,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("OpenCV").getContext("2d")).Doughnut(doughnutData);
+});
